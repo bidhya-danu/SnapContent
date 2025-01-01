2251780cbd39c58465947e25b4ad6c9189f94578
bidhya-danu <bidhyadanu1286@gmail.com>
2025-01-01 18:08:54 +0530

    preprocess.py
    added new component

--- preprocess.py
+++ preprocess.py
@@ -1,48 +1,85 @@
 import json
-
+from llm_helper import llm
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
-from llm_helper import llm
 
-def process_post(raw_file_path,processed_file_path="data/processed_post.json"):
-    enriched_posts=[]
+
+def process_posts(raw_file_path, processed_file_path=None):
     with open(raw_file_path, encoding='utf-8') as file:
-        posts=json.load(file)
+        posts = json.load(file)
+        enriched_posts = []
         for post in posts:
-            metadata=extract_metadata(post['text'])
-            post_with_metadata=post | metadata
+            metadata = extract_metadata(post['text'])
+            post_with_metadata = post | metadata
             enriched_posts.append(post_with_metadata)
-    
-    for epost in enriched_posts:
-        print(epost)
-            
-    
+
+    unified_tags = get_unified_tags(enriched_posts)
+    for post in enriched_posts:
+        current_tags = post['tags']
+        new_tags = {unified_tags[tag] for tag in current_tags}
+        post['tags'] = list(new_tags)
+
+    with open(processed_file_path, encoding='utf-8', mode="w") as outfile:
+        json.dump(enriched_posts, outfile, indent=4)
+
+
 def extract_metadata(post):
     template = '''
-    You are given a LinkedIn post. You need to extract the number of lines, language of the post, and tags.
-    1. Return only a valid JSON, no preamble.
-    2. The JSON object should only have 3 keys: line_count, language, and tags.
-    3. Tags should be an array of tags, and you should extract a maximum of 2 tags.
-    4. Language should be either English or Hinglish (Hinglish means a mix of Hindi and English).
-    5. Ensure all tags are identical. If there are similar tags like 'LinkedIn Scams' and 'Job Scams', combine them into a single tag like 'Scams'.
-    Here is the actual post on which you need to perform this task:
+    You are given a LinkedIn post. You need to extract number of lines, language of the post and tags.
+    1. Return a valid JSON. No preamble. 
+    2. JSON object should have exactly three keys: line_count, language and tags. 
+    3. tags is an array of text tags. Extract maximum two tags.
+    4. Language should be English or Hinglish (Hinglish means hindi + english)
+    
+    Here is the actual post on which you need to perform this task:  
     {post}
     '''
+
     pt = PromptTemplate.from_template(template)
     chain = pt | llm
-    response = chain.invoke(input={'post': post})
+    response = chain.invoke(input={"post": post})
 
     try:
         json_parser = JsonOutputParser()
         res = json_parser.parse(response.content)
-    except OutputParserException as e:
-        print(f"Error parsing JSON: {e}")
-        res = {"line_count": 0, "language": "Unknown", "tags": []}  # Default/fallback response
-    
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
     return res
+
+
+def get_unified_tags(posts_with_metadata):
+    unique_tags = set()
+    # Loop through each post and extract the tags
+    for post in posts_with_metadata:
+        unique_tags.update(post['tags'])  # Add the tags to the set
+
+    unique_tags_list = ','.join(unique_tags)
+
+    template = '''I will give you a list of tags. You need to unify tags with the following requirements,
+    1. Tags are unified and merged to create a shorter list. 
+       Example 1: "Jobseekers", "Job Hunting" can be all merged into a single tag "Job Search". 
+       Example 2: "Motivation", "Inspiration", "Drive" can be mapped to "Motivation"
+       Example 3: "Personal Growth", "Personal Development", "Self Improvement" can be mapped to "Self Improvement"
+       Example 4: "Scam Alert", "Job Scam" etc. can be mapped to "Scams"
+    2. Each tag should be follow title case convention. example: "Motivation", "Job Search"
+    3. Output should be a JSON object, No preamble
+    3. Output should have mapping of original tag and the unified tag. 
+       For example: {{"Jobseekers": "Job Search",  "Job Hunting": "Job Search", "Motivation": "Motivation}}
     
-   
-if __name__=="__main__":
-    process_post("data/raw_post.json","data/processed_post.json")
-    
+    Here is the list of tags: 
+    {tags}
+    '''
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"tags": str(unique_tags_list)})
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
+
+
+if __name__ == "__main__":
+    process_posts("data/raw_posts.json", "data/processed_posts.json")
